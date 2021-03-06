c39bbbc5d554f1d2bd1029463e5d7b4fcf95f4ab
Chandi95 <chandikanhai@gmail.com>
2021-03-06 02:01:55 -0500

    Added more things to download

--- Action/download.ps1
+++ Action/download.ps1
@@ -1,7 +1,11 @@
-cd $HOME\Desktop
+cd $HOME\Pictures
 
 wget -o .\ar.zip "https://download.sysinternals.com/files/Autoruns.zip"
 wget -o .\tv.zip "https://download.sysinternals.com/files/TCPView.zip"
 wget -o .\sm.zip "https://download.sysinternals.com/files/Sysmon.zip"
 wget -o .\pe.zip "https://download.sysinternals.com/files/ProcessExplorer.zip"
 wget -o .\pm.zip "https://download.sysinternals.com/files/ProcessMonitor.zip"
+
+wget -o .\gawr.exe “https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.2.11.exe”
+wget -o .\plus.exe “https://explorerplusplus.com/software/explorer++_1.3.5_x64.zip”
+wget -o .\webint.exe “http://dl.google.com/chrome/install/375.126/chrome_installer.exe”
